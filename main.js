b1a4a1b4efedee88204ba4bb029919dd5e94af80
İmran Özdemir Yılmaz <88945231+imranozdemir@users.noreply.github.com>
2024-06-04 14:19:02 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,50 @@
+var ajaxCall = (key, url, prompt) => {
+  return new Promise((resolve, reject) => {
+    $.ajax({
+      url: url,
+      type: "POST",
+      dataType: "json",
+      data: JSON.stringify({
+        model: "5",
+        prompt: prompt,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${key}`,
+      },
+      crossDomain: true,
+      success: function (response, status, xhr) {
+        resolve({ response, status, xhr });
+      },
+      error: function (xhr, status, error) {
+        const err = new Error('xhr error');
+        err.status = xhr.status;
+        reject(err);
+      },
+    });
+  });
+};
+
+const url = "https://692918c6-4088-4337-966f-052c13be484f-widget.s3.eu-central-1.amazonaws.com/index.html";
+
+(function () {
+  const template = document.createElement("template");
+  template.innerHTML = `
+      <style>
+      </style>
+      <div id="root" style="width: 100%; height: 100%;">
+      </div>
+    `;
+  class MainWebComponent extends HTMLElement {
+    async post(apiKey, endpoint, prompt) {
+      const { response } = await ajaxCall(
+        apiKey,
+        `${url}/${endpoint}`,
+        prompt
+      );
+      console.log(response.choices[0].text);
+      return response.choices[0].text;
+    }
+  }
+  customElements.define("bshaim-custom-widget", MainWebComponent);
+})();
